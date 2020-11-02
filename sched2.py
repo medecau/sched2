a1b1c665e1204ffd0fd4bab7b4191a4f3d99db82
Pedro Rodrigues <medecau@gmail.com>
2020-11-02 23:41:43 +0100

    Add the repeat method
    The repeat method has a signature similar identical to that of enter.
    But it will re-enter a call to the action every time it is ran.
    Behaving very similarly to a loop with a sleep interval.

---
+++ sched2.py
@@ -0,0 +1,17 @@
+from functools import partial
+import sched
+
+
+class scheduler(sched.scheduler):
+    def repeat(self, interval, priority, action, argument=None, kwargs=None):
+        """A variant of enter that re-enters itself."""
+        argument = argument if argument else ()
+        kwargs = kwargs if kwargs else {}
+
+        partial_action = partial(action, *argument, **kwargs)
+
+        def repeater(action):
+            action()
+            self.enter(interval, priority, repeater, (partial_action,))
+
+        s.enter(interval, priority, repeater, (partial_action,))
